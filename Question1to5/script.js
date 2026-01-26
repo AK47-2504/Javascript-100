d899df0700fcfa05e0e850e4a980debacfe431a8
AK47-2504 <anishkalambe990@gmail.com>
2026-01-26 23:44:29 +0530

    Solved 1 to 20 Questions

---
+++ Question1to5/script.js
@@ -0,0 +1,26 @@
+// Question 1
+// let name = "Anish Santosh Kalambe";
+// let hobby = "My hobby is to play volleyball"
+// console.log(`My name is ${name} and ${hobby}`)
+
+// Question 2
+// let a = 45;
+// let b = 2;
+// let c = 10;
+// let ans = a * b - c;
+// console.log(ans);
+
+// Question 3
+// let currentYear = new Date().getFullYear();
+// console.log(currentYear);
+
+// Question 4
+// let firstName = "Anish";
+// let lastName = "Kalambe";
+// console.log(`My FullName is ${firstName} ${lastName}`);
+
+//Question 5
+// let a = 12;
+// console.log(a);
+// a = 13;
+// console.log(a);
---
+++ Question5to20/script.js
@@ -0,0 +1,71 @@
+// Question 6
+// console.error("User Not Found")
+
+// Question 7
+// var a = 2;
+// console.log(a * a);
+
+// Question 8
+// let isLoggedIn = true
+// console.log(isLoggedIn);
+
+// Question 9
+// Store your age in a variable and print whether your age is greater than 18.
+// let age = 19;
+// if(age>=18) {
+//     console.log("You are an adult")
+// }
+// else {
+//     console.log("You are not an adult yet")
+// }
+
+// Question 10
+// console.log(100/0) -  Returns Infinity
+
+// Question 11
+// let a = 12
+// console.log(a)
+
+// Question 12
+// const PI = 3.14
+// console.log(PI)
+
+// Question 13
+// // Create a variable, print its value, update the value, and print the updated value again.
+// let name = "Anish";
+// console.log(name);
+
+// let newName = prompt("Enter New Name");
+// name = newName;
+// console.log(name);
+
+// Question 14
+// console.log(typeof(null)) - Returns Object
+
+// Question 15
+// Create a variable containing a numeric value as a string (e.g., `"25"`) and print its type
+// let a = "25";
+// console.log(typeof a); - Returns String
+
+// Question 16
+// let logginUser = true
+// console.log(typeof(logginUser)) - Returns Boolean
+
+// Question 17
+// let a = "Anish";
+// let b = 24;
+// let c = true;
+// console.log(a, b, c);
+
+// Question 18
+// let name;
+// console.log(typeof(name)) - Returns Undefined
+
+// Question 19
+// let name = undefined;
+// console.log(typeof name); - Returns Undefined
+
+// Question 20
+// const arr = [1,2,3,4,5,6,7,8,9]
+// console.log(arr) 
+

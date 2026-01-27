333b64e55562b3b5c7eeb8279dede3a3c12bb72f
AK47-2504 <anishkalambe990@gmail.com>
2026-01-27 23:05:34 +0530

    Solved 21 to 30 Questions

---
+++ Question21to30/script.js
@@ -0,0 +1,85 @@
+// Question 21
+// for(i=0; i<=50; i++){
+//     console.log(i)
+// }
+
+// Question 22
+// Using a `while` loop, calculate and print the sum of numbers from 1 to 10.
+// let i = 1;
+// sum = 0;
+// while (i <= 10) {
+//   sum = sum + i;
+//   i++;
+// }
+// console.log(sum)
+
+//Question 23
+// Iterate through each character of the string `"JavaScript"` using a `for...of` loop and print
+// each character.
+// const name = "Javascript";
+
+// for (const char of name) {
+//   console.log(char);
+// }
+
+// Question 24
+// Using a `for` loop, print all odd numbers from 1 to 20. Use `continue` to skip even numbers
+// for (i = 1; i < 20; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// Question 25
+//  Use a `do...while` loop to print the numbers from 5 down to 1.
+// let i = 5;
+// do {
+//     console.log(i)
+//   i--;
+// } while (i >= 1);
+
+// Question 26
+// Write a program to calculate the factorial of 5 using a loop and print the result.
+// let num = 5;
+// let factorial = 1;
+// for (let i = 1; i <= num; i++) {
+//   factorial = factorial * i;
+// }
+// console.log("Factorial of 5:", factorial);
+
+// Question 27
+// Use nested `for` loops to print a 3×3 grid pattern where each row contains the numbers 1 to 3
+// for (let i = 1; i <= 3; i++) {
+//   let row = "";
+//   for (let j = 1; j <= 3; j++) {
+//     row += j + " ";
+//   }
+//   console.log(row);
+// }
+
+// Question 28
+// const arr = [1, 2, 3, 4, 5];
+// const reversed = [];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   reversed.push(arr[i]);
+// }
+// console.log(reversed);
+
+// Question 29
+// i = 1;
+// while (i <= 100) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// Question 30
+// let obj = {
+//   name: "Anish",
+//   age: 21,
+// };
+// for (const key in obj) {
+//   console.log(key, obj[key])
+// }

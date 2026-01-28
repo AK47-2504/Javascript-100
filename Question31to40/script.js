ec497cb6f92bcb05c84f0dc908e7601c0f33dc1d
AK47-2504 <anishkalambe990@gmail.com>
2026-01-28 20:32:09 +0530

    31 to 40 Question Solved

---
+++ Question31to40/script.js
@@ -0,0 +1,70 @@
+// Question 31
+// Create an array of movie names and print them in a single line separated by ` - ` using `join()`
+// let movies = ["DDLJ", "Sultan", "PK", "Ready", "Dhurandhar"];
+// console.log(movies.join(" - "))
+
+// Question 32
+// Create an array of numbers and print the value at index 1.
+// let num = [1,2,3,4,5,6,7,8,9]
+// console.log(num[1])
+
+// Question 33
+//  Add two elements to the beginning of an array using `unshift()` and print the updated array.
+// let num = [1,2,3,4,5,6,7,8,9]
+// num.unshift(-1, 0)
+// console.log(num)
+
+// Question 34
+// Create an array of song names, remove the last element using `pop()`, and print the remaining elements.
+// let movies = ["DDLJ", "Sultan", "PK", "Ready", "Dhurandhar"];
+// movies.pop();
+// console.log(movies);
+
+// Question 35
+// Given an array, extract the first three elements using `slice()` and print them.
+// let arr = [];
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// arr = num.slice(0, 3);
+// console.log(arr);
+
+// Question 36
+// Create an array of numbers and find the index of the number `5` using `indexOf()`.
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(num.indexOf(5))
+
+// Question 37
+// Create an array of values and use `includes()` to check whether the value `3` exists in the array.
+// Print the result.
+// let val = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// if (val.includes(3)) {
+//     console.log("Yes it Exits")
+// } else {
+//     console.log("It Does not Exits")
+// }
+
+// Question 38
+//  Concatenate two arrays using `concat()` and print the resulting combined array.
+// let concatarr = [];
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let movies = ["DDLJ", "Sultan", "PK", "Ready", "Dhurandhar"];
+// concatarr = num.concat(movies);
+// console.log(concatarr);
+
+// Question 39
+// Sort an array of numbers in ascending order using a custom comparator function and print
+// the sorted array.
+// let arr = [23, 66, 77, 44, 33, 65, 89, 34, 12, 98];
+// let sorted = arr.sort((a, b) => b - a);
+// console.log(sorted);
+
+// Question 40
+// Create a copy of an array using the spread operator (`...`). Print the copied array and also
+// print whether the copied array and original array reference the same memory.
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let copy = [...num]
+// console.log(num)
+// console.log(copy)
+// console.log(`Same memory reference - `,num === copy)
+
+
+

7a27a780e6e5f4f7ce3c541bdeed1454cc5089ca
AK47-2504 <anishkalambe990@gmail.com>
2026-01-29 22:02:51 +0530

    Solved 31 to 40 Question

---
+++ Question41to50/script.js
@@ -0,0 +1,82 @@
+// Question 41|
+// Write a function that takes a number as input and returns whether it is "even" or "odd".
+// let num = prompt("Enter a number");
+// function oddOrEven() {
+//   if (num % 2 === 0) {
+//     console.log(`${num} this is a Even Number`);
+//   } else {
+//     console.log(`${num} this is a Odd Number`);
+//   }
+// }
+// oddOrEven();
+
+// Question 42
+// Create a function that calculates and returns the area of a circle given its radius.
+// const pie = 3.14;
+// let radius = prompt("Enter Radius Of Circle");
+// function AOC() {
+//   totalAreaofCircle = pie * radius * radius;
+//   console.log(totalAreaofCircle);
+// }
+// AOC();
+
+// Question 43
+// Write a function that takes an array of numbers and returns the sum of all elements in the array.
+// function sumArray(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+// console.log(sumArray([1, 2, 3, 4]));
+
+// Question 44
+// Write a function that checks whether a given string starts with a specified character. Return `true` or `false`.
+// function specificChar(string, ch) {
+//   console.log(string[0] === ch);
+// }
+// specificChar("Anish", "A");
+
+// Question 45
+// Create a function that takes two numbers and returns the larger of the two.
+// function greaterNum(a, b) {
+//   return console.log(a > b ? a : b)
+// }
+// greaterNum(2, 3);
+
+// Question 46
+// Write a recursive function that calculates and returns the factorial of a given number.
+// function factorial(n) {
+//   if (n === 0 || n === 1) return 1;
+//   return n * factorial(n - 1);
+// }
+// console.log(factorial(5));
+
+// Question 47
+// Write a function that reverses a string and returns the reversed version.
+// function reverseString(str) {
+//   console.log(str.split("").reverse().join(""));
+// }
+// reverseString("Anish");
+
+// Question 48
+//  Create a function that accepts an array of numbers and returns the largest number in the array.
+// function largestNum(arr) {
+// console.log(Math.max(...arr))
+// }
+// largestNum([1,2,33,4,5]);
+
+// Question 49
+// Write a function that converts a string into kebab-case (all lowercase words separated by hyphens).
+// function kebab (name) {
+//     console.log(name.split("").join("-"))
+// }
+// kebab("Anish")
+
+// Question 50
+// Write a function named `helloWorld` that prints "Hello World" to the console.
+// function helloWorld() {
+//   return console.log("hello World");
+// }
+// helloWorld();

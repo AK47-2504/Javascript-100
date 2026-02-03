7655f1fdea34c839fca2cefd1c81aaeba5242b99
AK47-2504 <anishkalambe990@gmail.com>
2026-02-03 23:03:56 +0530

    Solved 71 to 80 Question

---
+++ Question71to80/script.js
@@ -0,0 +1,63 @@
+// Question 71
+//  Create a string and print its length using the `.length` property.
+// let str = "Anish";
+// console.log(str.length);
+
+// Question 72
+// Print the last four characters of a string using the `.slice()` method with a negative index.
+// let str = "Anish";
+// console.log(str.slice(-4));
+// console.log(str.slice(1, 5));
+
+// Question 73
+// Convert a string to lowercase using `.toLowerCase()` and print the result.
+// let str = "JAVASCRIPT";
+// console.log(str.toLowerCase());
+
+// Question 74
+// Split a string into an array using a space `" "` as the delimiter and print the result.
+// let str = "HTML CSS JAVASCRIPT LOVE YOU";
+// console.log(str.split(" "));
+
+// Question 75
+// Find and print the index of a specific character (e.g., `"A"`) inside a string using `.indexOf()`.
+// let str = "JAVASCRIPT";
+// console.log(str.indexOf("A"));
+
+// Question 76
+// Replace a word inside a string with another word using `.replace()` and print the updated string.
+// let str = "HTML CSS JAVASCRIPT LOVE YOU";
+// console.log(str.replace("HTML", "JSX"));
+
+// Question 77
+// Repeat a string three times using `.repeat()` and print the result.
+// let str = "HTML CSS JAVASCRIPT LOVE YOU";
+// console.log(str.repeat(3));
+
+// Question 78
+// Write a function that checks whether a given word exists inside a string using `.includes()`.
+// let str = "Dhurandhar";
+// function check(str) {
+//   console.log(str.includes("a"));
+// }
+// check(str);
+
+// Question 79
+// Remove all spaces from a string by splitting and rejoining it, then print the result.
+// let str = "HTML CSS JAVASCRIPT LOVE YOU";
+// console.log(str.split(" ").join(""));
+
+// Question 80
+// Write a function that counts and returns the number of vowels in a given string.
+// function countVowels(str) {
+//   let count = 0;
+//   let vowels = "aeiou";
+//   str = str.toLowerCase();
+//   for (let ch of str) {
+//     if (vowels.includes(ch)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// console.log(countVowels("Anish")); // 3
